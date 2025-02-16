cf790692b299c9210b76674b0bdd3591112088d4
Midestic <badmususman50@gmail.com>
2025-02-16 01:39:32 +0100

    overflow

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <>
+    <div className="App">
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
 
       <div
@@ -29,7 +29,7 @@ function App() {
         <Projects />
         <Contact />
       </div>
-    </>
+    </div>
   );
 }
 
